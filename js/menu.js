47a71d5b545aab6bd4caae674af11ffde05ed73b
Janprakash Joshi <88995621+Janprakash-Joshi@users.noreply.github.com>
2024-01-04 21:13:53 +0545

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,28 @@
 var showMenu = document.getElementById('showMenu');
 var menu = document.getElementById('menu');
+var myCarousel=document.getElementById('myCarousel');
+var scrollableDiv=document.getElementById('scrollableDiv');
 var counter=0;
 showMenu.onclick = () => {
   if(counter==0){
     menu.classList.add('flexIMP');
+    if(myCarousel){
+      myCarousel.style.display='none';
+    }
+    if(scrollableDiv){
+      scrollableDiv.style.display='none';
+    }
+    
     counter++;
   }
   else{
     menu.classList.remove('flexIMP');
+    if(myCarousel){
+      myCarousel.style.display='block';
+    }
+    if(scrollableDiv){
+      scrollableDiv.style.display='flex';
+    }
     counter--;
   }
   
