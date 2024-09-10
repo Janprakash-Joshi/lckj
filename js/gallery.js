89cc81cd919c1e90245cf62b7c94067da00d4f65
Janprakash Joshi <88995621+Janprakash-Joshi@users.noreply.github.com>
2024-09-10 15:36:44 +0545

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,44 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const galleryContainer = document.getElementById("galleryContainer");
+  const galleryContainer = document.getElementById("galleryContainer");
 
-    const imageCount = 45; 
-    const pngCount = 3;   
-    const videoCount = 0; 
+  const imageCount = 49;
+  const pngCount = 3;
+  const videoCount = 0;
 
-    // Loop for images
-    for (let i = imageCount; i >= 1; i--) {
-        const img = createGalleryItem('image', `/gallery/jpg/${i}.jpg`);    
-        galleryContainer.appendChild(img);
-    }
+  // Loop for images
+  for (let i = imageCount; i >= 1; i--) {
+    const img = createGalleryItem("image", `/gallery/jpg/${i}.jpg`);
+    galleryContainer.appendChild(img);
+  }
 
-    // Loop for PNGs
-    for (let i = pngCount; i >= 1; i--) {
-        const img = createGalleryItem('image', `/gallery/png/${i}.png`);
-        galleryContainer.appendChild(img);
-    }
+  // Loop for PNGs
+  for (let i = pngCount; i >= 1; i--) {
+    const img = createGalleryItem("image", `/gallery/png/${i}.png`);
+    galleryContainer.appendChild(img);
+  }
 
-    // Loop for videos
-    for (let i = videoCount; i >=1; i--) {
-        const video = createGalleryItem('video', `/gallery/mp4/${i}.mp4`);
-        galleryContainer.appendChild(video);
-    }
+  // Loop for videos
+  for (let i = videoCount; i >= 1; i--) {
+    const video = createGalleryItem("video", `/gallery/mp4/${i}.mp4`);
+    galleryContainer.appendChild(video);
+  }
 });
 
 function createGalleryItem(type, url) {
-    const galleryItem = document.createElement("div");
-    galleryItem.classList.add("gallery-item");
+  const galleryItem = document.createElement("div");
+  galleryItem.classList.add("gallery-item");
 
-    if (type === "image") {
-        const img = document.createElement("img");
-        img.src = url;
-        img.alt="Leo Club Of Kathmandu Janmabhumi";
-        galleryItem.appendChild(img);
-    } else if (type === "video") {
-        const video = document.createElement("video");
-        video.src = url;
-        video.controls = true;
-        galleryItem.appendChild(video);
-    }
+  if (type === "image") {
+    const img = document.createElement("img");
+    img.src = url;
+    img.alt = "Leo Club Of Kathmandu Janmabhumi";
+    galleryItem.appendChild(img);
+  } else if (type === "video") {
+    const video = document.createElement("video");
+    video.src = url;
+    video.controls = true;
+    galleryItem.appendChild(video);
+  }
 
-    return galleryItem;
+  return galleryItem;
 }
